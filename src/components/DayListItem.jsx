703a752304c7e9e0c6471cc1718a353349292c9b
Brett Bilyk <brettbilyk@gmail.com>
2021-11-09 14:31:52 -0700

    Rename files from js to jsx (person taste reason).  Add props to DayListItem

---
+++ src/components/DayListItem.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function DayListItem(props) {
+  const {name, spots, selected, setDay} = props;
+  
+
+  return (
+    <li selected={selected} onClick={() => setDay(name)}>
+      <h2 className="text--regular">{name}</h2>
+      <h3 className="text--light">{spots} spots remaining</h3>
+    </li>
+  );
+}
