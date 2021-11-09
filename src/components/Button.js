0d6bcb2ba5a7a4f7ec4570cf8fcd002ab0ddc41b
Brett Bilyk <brettbilyk@gmail.com>
2021-11-08 18:15:39 -0700

    Add classNames module as well as props to button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,14 @@
 import React from "react";
-
+import classNames from "classnames";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   const {children, confirm, danger, disabled, onClick} = props
+   let buttonClass = classNames("button", {"button--confirm": confirm, "button--danger": danger})
+
+   return (
+      <button className={buttonClass} onClick={onClick} disabled={disabled}>
+         {children}
+      </button>
+   )
 }
