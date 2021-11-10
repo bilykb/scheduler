201b02e7da72f6917755256e86a6f1c9b7796149
Brett Bilyk <brettbilyk@gmail.com>
2021-11-09 17:09:43 -0700

    Update conditional short circuit for children

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames"
+
 
 export default function DayListItem(props) {
-  const {name, spots, selected, setDay} = props;
-  
+  let dayClass = classNames("day-list__item", {"day-list__item--selected": props.selected, "day-list__item--full": !props.spots})
 
   return (
-    <li selected={selected} onClick={() => setDay(name)}>
-      <h2 className="text--regular">{name}</h2>
-      <h3 className="text--light">{spots} spots remaining</h3>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      {props.spots > 1 && <h3 className="text--light">{props.spots} spots remaining</h3>}
+      {!props.spots && <h3 className="text--light">no spots remaining</h3>}
+      {props.spots === 1 && <h3 className="text--light">{props.spots} spot remaining</h3>}
     </li>
   );
 }
