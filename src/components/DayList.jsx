24eaba3d9447a1ccd5f5e5ab4eadbd1a360bb75e
Brett Bilyk <brettbilyk@gmail.com>
2021-11-11 12:57:42 -0700

    Create InterviewerList component for InterviewerListItem.jsx
    InterviewerListItem needs a container

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import DayListItem from "./DayListItem"
 
 export default function DayList(props) {
+
   const dayArray = props.days.map(day => {
     return(<DayListItem key={day.id} name={day.name} setDay={props.setDay} spots={day.spots} selected={day.name === props.day} />)
     });
+    
   return dayArray;
 }
