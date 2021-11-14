1e3c62f1325824a02937e27369319694929e43eb
Brett Bilyk <brettbilyk@gmail.com>
2021-11-13 20:29:34 -0700

    Update default import to named import

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import classNames from "classnames";
 import "components/Button.scss";
 
-export default function Button(props) {
-   const {children, confirm, danger, disabled, onClick} = props
+export function Button(props) {
+   
+   const {children, confirm, danger, disabled, onChange} = props
    let buttonClass = classNames("button", {"button--confirm": confirm, "button--danger": danger})
 
    return (
-      <button className={buttonClass} onClick={onClick} disabled={disabled}>
+      <button className={buttonClass} onClick={onChange} disabled={disabled}>
          {children}
       </button>
    )
