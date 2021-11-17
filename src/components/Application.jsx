1c7f1080c1638157cc880ed3bce7b544e6aa0bbb
Brett Bilyk <brettbilyk@gmail.com>
2021-11-16 18:39:23 -0700

    Implement api get request for days information

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,26 +1,8 @@
-import React, {useState} from "react";
-
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 import "components/Application.scss";
 import DayList from './DayList'
 import Appointment from './Appointment'
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   "1": {
@@ -63,7 +45,18 @@ const appointments = {
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday")
+  const [week, setWeek] = useState([]);
+  // api might only return a few days of the week
   const appointmentArray = Object.values(appointments).map(appointment => <Appointment key={appointment.id} {...appointment} />)
+
+  useEffect(() => {
+    const weekRoute = 'http://localhost:8001/api/days';
+    axios.get(weekRoute)
+      .then(res => {
+        setWeek(res.data)
+      })
+      .catch(err => console.log("There was an error", err))
+    }, [])
 
   return (
     <main className="layout">
@@ -76,7 +69,7 @@ export default function Application(props) {
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
         <DayList
-          days={days}
+          days={week}
           value={day}
           onChange={setDay}
         />
