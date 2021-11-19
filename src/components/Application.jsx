91cbc2ba2ac363cfd4b55d7dda93291fa5e800f0
Brett Bilyk <brettbilyk@gmail.com>
2021-11-18 17:48:09 -0700

    Add 'getAppointments' selector with map function for Appointment component

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import "components/Application.scss";
 import DayList from './DayList'
 import Appointment from './Appointment'
-import {getAppointmentsForDay} from '../helpers/selectors'
+import {getAppointmentsForDay, getInterview} from '../helpers/selectors'
 
 
 export default function Application(props) {
@@ -27,8 +27,19 @@ export default function Application(props) {
       })
   }, [])
 
-  const dailyAppointments = getAppointmentsForDay(state, state.day)
-  const appointmentArray = dailyAppointments.map(appointment => <Appointment key={appointment.id} {...appointment} />)
+  const dailyAppointments = getAppointmentsForDay(state, state.day) // selector returns appointments for each day
+
+  const schedule = dailyAppointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview);
+    
+    return (
+    <Appointment key={appointment.id} 
+    id={appointment.id} 
+    time={appointment.time} 
+    interview={interview} 
+    />
+    )
+  })
 
   return (
     <main className="layout">
@@ -53,7 +64,7 @@ export default function Application(props) {
       />
       </section>
       <section className="schedule">
-        {appointmentArray}
+        {schedule}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
