019d120b7275bafeddef426614d749427eb3df23
Brett Bilyk <brettbilyk@gmail.com>
2021-12-06 15:49:15 -0700

    Adapted short circuit to function

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -8,12 +8,16 @@ export default function DayListItem(props) {
 
   let dayClass = classNames("day-list__item", {"day-list__item--selected": props.selected, "day-list__item--full": !props.spots})
 
+  const formatSpots = number => {
+    if(number === 0) return "no spots remaining"
+    if(number === 1) return "1 spot remaining"
+    if(number > 1) return `${number} spots remaining`
+  }
+
   return (
-    <li className={dayClass} onClick={props.onChange}>
+    <li className={dayClass} onClick={props.onChange} data-testid="day">
       <h2 className="text--regular">{props.name}</h2>
-      {props.spots > 1 && <h3 className="text--light">{props.spots} spots remaining</h3>}
-      {!props.spots && <h3 className="text--light">no spots remaining</h3>}
-      {props.spots === 1 && <h3 className="text--light">{props.spots} spot remaining</h3>}
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 };
